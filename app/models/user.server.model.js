c69c8007e8ac576b7c885600c9eb2e1052563b01
Dan Hedges <daniel.a.hedges@gmail.com>
2015-09-16 20:22:18 -0500

    Added fancy features to User schema.

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -5,8 +5,27 @@ var UserSchema = new Schema ({
   firstName: String,
   lastName: String,
   email: String,
-  username: String,
-  password: String
+  username: {
+    type: String,
+    trim: true,
+    unique: true,
+    required: true
+  },
+  password: String,
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
+
+UserSchema.virtual('fullName').get(function() {
+  return this.firstName + ' ' + this.lastName;
+}).set(function(fullName) {
+  var splitName = fullName.split(' ');
+  this.firstName = splitName[0] || '';
+  this.lastName = splitName[1] || '';
+});
+
+UserSchema.set('toJSON', { getters: true, virtuals: true });
 
 mongoose.model('User', UserSchema);
